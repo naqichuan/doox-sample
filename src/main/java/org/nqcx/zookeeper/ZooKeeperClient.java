cf574f6a200f0afccaa5c76fc4278ab93234231f
Naqi Chuan <naqichuan@163.com>
2017-12-14 15:34:17 +0800

    Sample

---
+++ src/main/java/org/nqcx/zookeeper/ZooKeeperClient.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2017 nqcx.org All right reserved. This software is the confidential and proprietary information
+ * of nqcx.org ("Confidential Information"). You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you entered into with nqcx.org.
+ */
+
+package org.nqcx.zookeeper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZooKeeperClient {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger("ZooKeeperClient");
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 2000000; i++) {
+            LOGGER.info("id: {}, sdp2lk;dkjl;afd;kllad;kljadspiowqpoidspljkasdpoijdsapjkladspkojadspkjasdppjadsf", i);
+            LOGGER.warn("id: {}, sdp2lk;dkjl;afd;kllad;kljadspiowqpoidspljkasdpoijdsapjkladspkojadspkjasdppjadsf", i);
+            LOGGER.error("id: {}, sdp2lk;dkjl;afd;kllad;kljadspiowqpoidspljkasdpoijdsapjkladspkojadspkjasdppjadsf", i);
+        }
+    }
+}
